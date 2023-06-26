7d557ed448e8f5548518aa424b5ae57628218c6b
Ryan Roche <mnryanbr@gmail.com>
2023-06-26 16:56:02 -0500

    Initial Commit

---
+++ Untitled Imgmagick Frontend/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Untitled Imgmagick Frontend
+//
+//  Created by Ryan Roche on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Untitled Imgmagick Frontend/Untitled_Imgmagick_FrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Untitled_Imgmagick_FrontendApp.swift
+//  Untitled Imgmagick Frontend
+//
+//  Created by Ryan Roche on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Untitled_Imgmagick_FrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
