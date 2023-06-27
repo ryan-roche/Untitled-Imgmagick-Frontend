b0067753ccc71209924521a6c3b9b24c0ad2e885
Ryan Roche <mnryanbr@gmail.com>
2023-06-27 13:54:37 -0500

    initial commit

--- Untitled Imgmagick Frontend/ContentView.swift
+++ Untitled Imgmagick Frontend/ContentView.swift
@@ -7,15 +7,33 @@
 
 import SwiftUI
 
+struct magickOperation: Hashable {
+    let name: String
+    let imageName: String
+}
+
+struct selectedImageFile: Hashable {
+    let filename: String
+    let path: String
+}
+
 struct ContentView: View {
+    let operations: [magickOperation] = [
+        .init(name: "Convert", imageName: "globe"),
+        .init(name: "Convert", imageName: "globe"),
+        .init(name: "Convert", imageName: "globe"),
+        .init(name: "Convert", imageName: "globe"),
+        .init(name: "Convert", imageName: "globe")
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(operations, id: \.self) {operation in
+            HStack(content: {
+                Image(systemName: operation.imageName)
+                    .foregroundColor(Color.accentColor)
+                Text(operation.name)
+            })
         }
-        .padding()
     }
 }
 
